2a063a48e36420f49370b2366b36c602392998af
Jackson Zhang <zbcjackson@gmail.com>
2013-08-14 11:32:34 +0800

    Depth first for FizzBuzz

--- fizz-buzz/fizz_buzz.rb
+++ fizz-buzz/fizz_buzz.rb
@@ -1,15 +1,10 @@
 class FizzBuzz
 	def sequence number
-		(1..number).to_a.map! do |n| 
-			if n % 15 == 0
-				"FizzBuzz"
-			elsif n % 3 == 0
-				"Fizz"
-			elsif n % 5 == 0
-				"Buzz"
-			else
-				n
-			end
+		(1..number).to_a.map do |n|
+			n = "FizzBuzz" if n % 15 == 0
+			n = "Fizz" if n % 3 == 0
+			n = "Buzz" if n % 5 == 0
+			n
 		end
 	end
 end
--- fizz-buzz/spec/fizz_buzz_spec.rb
+++ fizz-buzz/spec/fizz_buzz_spec.rb
@@ -2,26 +2,39 @@ require_relative '../fizz_buzz'
 
 describe "Fizz buzz" do
 	let(:game) {FizzBuzz.new}
-
-	it "should prints 1 for 1" do
-		game.sequence(1).should == [1]
+	describe "prints number itself" do
+		it "should print 1 for 1" do
+			game.sequence(1)[0].should == 1
+		end
+		it "should print 2 for 2" do
+			game.sequence(2)[1].should == 2
+		end
 	end
-	it "should prints 1, 2 for 2" do
-		game.sequence(2).should == [1, 2]
+	describe "prints Fizz for multiples of 3" do
+		it "should print Fizz for 3" do
+			game.sequence(3)[2].should == "Fizz"
+		end
+		it "should print Fizz for 6" do
+			game.sequence(6)[5].should == "Fizz"
+		end
 	end
-	it "should prints 1, 2, Fizz for 3" do
-		game.sequence(3).should == [1, 2, "Fizz"]
+	describe "prints Buzz for multiples of 5" do
+		it "should print Buzz for 5" do
+			game.sequence(5)[4].should == "Buzz"
+		end
+		it "should print Buzz for 10" do
+			game.sequence(10)[9].should == "Buzz"
+		end
 	end
-	it "should prints 1, 2, Fizz, 4, Buzz for 5" do
-		game.sequence(5).should == [1, 2, "Fizz", 4, "Buzz"]
+	describe "prints FizzBuzz for multiples of both 3 and 5" do
+		it "should print FizzBuzz for 15" do
+			game.sequence(15)[14].should == "FizzBuzz"
+		end
+		it "should print FizzBuzz for 30" do
+			game.sequence(30)[29].should == "FizzBuzz"
+		end
 	end
-	it "should prints 1, 2, Fizz, 4, Buzz, Fizz for 6" do
-		game.sequence(6).should == [1, 2, "Fizz", 4, "Buzz", "Fizz"]
-	end
-	it "should prints 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz for 10" do
+	it "should print all numbers below 10" do
 		game.sequence(10).should == [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
-	end
-	it "should prints FizzBuzz for multiples of both 3 and 5" do
-		game.sequence(15)[14].should == "FizzBuzz"
 	end
 end
