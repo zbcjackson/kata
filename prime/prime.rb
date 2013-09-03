8821c7c011416cf63370c6bc43da2df25fea9101
Jackson Zhang <zbcjackson@gmail.com>
2013-09-03 13:46:40 +0800

    Prime factor kata by different case path

--- prime/prime.rb
+++ prime/prime.rb
@@ -1,11 +1,14 @@
 class Prime
 	def factors number
 		factors = []
-		(2..number).each do |candidate|
+		(2..Math.sqrt(number)).each do |candidate|
 			while number % candidate == 0
 				factors << candidate
 				number /= candidate
 			end
+		end
+		if number > 1
+			factors << number
 		end
 		factors
 	end
